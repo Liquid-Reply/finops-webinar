984506a80127424128b65e3c661c95e1c6199c22
Florian Stoeber <f.stoeber@reply.de>
2021-08-23 11:04:46 +0200

    Remove ip ranges vars

--- infracost/main.tf
+++ infracost/main.tf
@@ -39,10 +39,10 @@ module "gke" {
   region                      = var.region
   network                     = var.network
   subnetwork                  = var.subnetwork
-  ip_range_pods               = var.ip_range_pods
-  ip_range_services           = var.ip_range_services
+  #ip_range_pods               = var.ip_range_pods
+  #ip_range_services           = var.ip_range_services
   create_service_account      = false
-  service_account             = var.compute_engine_service_account
+  #service_account             = var.compute_engine_service_account
   enable_binary_authorization = var.enable_binary_authorization
   skip_provisioners           = var.skip_provisioners
 }
