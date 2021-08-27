e1b225ed5c1678d398d8c5e93ce0ec80efacc5c8
Florian Stoeber <f.stoeber@reply.de>
2021-08-27 16:59:45 +0200

    Change cluster config

--- infracost/main.tf
+++ infracost/main.tf
@@ -31,51 +31,130 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
+# module "gke" {
+#   source                      = "terraform-google-modules/kubernetes-engine/google"
+#   project_id                  = var.project_id
+#   name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
+#   regional                    = true
+#   region                      = var.region
+#   network                     = var.network
+#   subnetwork                  = var.subnetwork
+#   ip_range_pods               = var.ip_range_pods
+#   ip_range_services           = var.ip_range_services
+#   create_service_account      = true
+#   enable_binary_authorization = var.enable_binary_authorization
+#   skip_provisioners           = var.skip_provisioners
+#     node_pools = [
+#     {
+#       name            = "pool-01"
+#       min_count       = 1
+#       max_count       = 2
+#       auto_upgrade    = true
+#     },
+#     {
+#       name            = "pool-02"
+#       min_count       = 1
+#       max_count       = 2
+#       auto_upgrade    = true
+#     }
+#   ]
+
+#   node_pools_labels = {
+#     all = {
+#       all-pools-example = true
+#     }
+#     pool-01 = {
+#       pool-01-example = true
+#       owner = "fstoeber"
+#     }
+#   }
+
+#   node_pools_tags = {
+#     all = [
+#       "all-node-example",
+#     ]
+#     pool-01 = [
+#       "pool-01-example",
+#     ]
+#   }
+
+# }
+
 module "gke" {
-  source                      = "terraform-google-modules/kubernetes-engine/google"
-  project_id                  = var.project_id
-  name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
-  regional                    = true
-  region                      = var.region
-  network                     = var.network
-  subnetwork                  = var.subnetwork
-  ip_range_pods               = var.ip_range_pods
-  ip_range_services           = var.ip_range_services
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.project_id
+  name                       = "gke-test-1"
+  region                     = "us-central1"
+  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
+  network                    = "vpc-01"
   create_service_account      = true
-  enable_binary_authorization = var.enable_binary_authorization
-  skip_provisioners           = var.skip_provisioners
-    node_pools = [
+  subnetwork                 = "us-central1-01"
+  ip_range_pods              = "us-central1-01-gke-01-pods"
+  ip_range_services          = "us-central1-01-gke-01-services"
+  http_load_balancing        = false
+  horizontal_pod_autoscaling = true
+  network_policy             = false
+
+  node_pools = [
     {
-      name            = "pool-01"
-      min_count       = 1
-      max_count       = 2
-      auto_upgrade    = true
+      name                      = "default-node-pool"
+      machine_type              = "e2-medium"
+      node_locations            = "us-central1-b,us-central1-c"
+      min_count                 = 1
+      max_count                 = 100
+      local_ssd_count           = 0
+      disk_size_gb              = 100
+      disk_type                 = "pd-standard"
+      image_type                = "COS"
+      auto_repair               = true
+      auto_upgrade              = true
+      preemptible               = false
+      initial_node_count        = 80
     },
-    {
-      name            = "pool-02"
-      min_count       = 1
-      max_count       = 2
-      auto_upgrade    = true
-    }
   ]
 
+  node_pools_oauth_scopes = {
+    all = []
+
+    default-node-pool = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
   node_pools_labels = {
-    all = {
-      all-pools-example = true
-    }
-    pool-01 = {
-      pool-01-example = true
+    all = {}
+
+    default-node-pool = {
+      default-node-pool = true
       owner = "fstoeber"
     }
   }
 
-  node_pools_tags = {
-    all = [
-      "all-node-example",
-    ]
-    pool-01 = [
-      "pool-01-example",
+  node_pools_metadata = {
+    all = {}
+
+    default-node-pool = {
+      node-pool-metadata-custom-value = "my-node-pool"
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    default-node-pool = [
+      {
+        key    = "default-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
     ]
   }
 
+  node_pools_tags = {
+    all = []
+
+    default-node-pool = [
+      "default-node-pool",
+    ]
+  }
 }
