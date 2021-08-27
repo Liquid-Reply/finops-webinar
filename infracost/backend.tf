e011e544c92318b95f8603e981aca2e426094350
Florian Stoeber <f.stoeber@reply.de>
2021-08-27 14:42:24 +0200

    Debug

--- infracost/backend.tf
+++ infracost/backend.tf
@@ -1,6 +1,8 @@
 terraform {
   backend "http" {
     address = "https://gitlab.com/api/v4/projects/29054924/terraform/state/example-production"
+    username = "gitlab-ci-token"
+    password = "${CI_JOB_TOKEN}"
   }
   required_providers {
     aws = {
