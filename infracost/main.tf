e446d1e4443969da6fb31713c009f990335525f9
Florian Stoeber <f.stoeber@reply.de>
2021-08-28 16:30:25 +0200

    Debug

--- infracost/main.tf
+++ infracost/main.tf
@@ -94,23 +94,8 @@ resource "google_compute_instance" "instance_without-owner-label" {
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
   network_interface {
     network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
   }
 
   service_account {
@@ -128,23 +113,9 @@ resource "google_compute_instance" "instance_with-owner-label" {
   labels       = {
     owner = "fstoeber"
   }
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
 
   network_interface {
     network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
   }
 
   service_account {
