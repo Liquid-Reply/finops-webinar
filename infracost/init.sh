30b430374555d7057cdc6924deffad0e278db5ca
Florian Stoeber <f.stoeber@reply.de>
2021-08-30 12:57:20 +0200

    Fix

--- infracost/init.sh
+++ infracost/init.sh
@@ -1,9 +1,9 @@
 terraform init -migrate-state \
-    -backend-config="address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>" \
-    -backend-config="lock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>/lock" \
-    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/<YOUR-PROJECT-ID>/terraform/state/<YOUR-PROJECT-NAME>/lock" \
-    -backend-config="username=<YOUR-USERNAME>" \
-    -backend-config="password=<YOUR-ACCESS-TOKEN>" \
+    -backend-config="address=https://gitlab.com/api/v4/projects/29054924/terraform/state/example-production" \
+    -backend-config="lock_address=https://gitlab.com/api/v4/projects/29054924/terraform/state/example-production/lock" \
+    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/29054924/terraform/state/example-production/lock" \
+    -backend-config="username=gitlab-ci-token" \
+    -backend-config="password=${CI_JOB_TOKEN}" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
     -backend-config="retry_wait_min=5"
