3b7832e8797d5888f91aecd153084446a4c90b7d
Florian Stoeber <f.stoeber@reply.de>
2022-03-15 10:18:35 +0100

    Increase nodes in pool-02 (#6)
    * Increase nodes in pool-02
    
    * Update

--- infracost/main.tf
+++ infracost/main.tf
@@ -53,8 +53,8 @@ module "gke" {
     },
     {
       name            = "pool-02"
-      min_count       = 1
-      max_count       = 1
+      min_count       = 5
+      max_count       = 5
       auto_upgrade    = true
     }
   ]
