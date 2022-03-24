43e269092256662c315f56a23520848cfcd190b8
Florian Stoeber <f.stoeber@reply.de>
2022-03-24 14:41:23 +0100

    Update main.tf

--- infracost/main.tf
+++ infracost/main.tf
@@ -53,8 +53,8 @@ module "gke" {
     },
     {
       name            = "pool-02"
-      min_count       = 5
-      max_count       = 5
+      min_count       = 1
+      max_count       = 1
       auto_upgrade    = true
     }
   ]
